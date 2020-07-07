b436a5136b42ee95d0ffd480723e53a96b287eae
Neon <36834975+Waterbearbear@users.noreply.github.com>
2020-07-07 19:21:12 +0800

    Update model_axial.py

--- code/model/model_axial.py
+++ code/model/model_axial.py
@@ -8,38 +8,55 @@ import torchvision.models as models
 from datasets import transform
 import torch.nn.functional as F
 
-
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=0.25, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
+class focal_loss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2, num_classes = 3, size_average=True):
+        """
+        focal_loss损失函数, -α(1-yi)**γ *ce_loss(xi,yi)
+        步骤详细的实现了 focal_loss损失函数.
+        :param alpha:   阿尔法α,类别权重.      当α是列表时,为各类别权重,当α为常数时,类别权重为[α, 1-α, 1-α, ....],常用于 目标检测算法中抑制背景类 , retainnet中设置为0.25
+        :param gamma:   伽马γ,难易样本调节参数. retainnet中设置为2
+        :param num_classes:     类别数量
+        :param size_average:    损失计算方式,默认取均值
+        """
+        super(focal_loss,self).__init__()
         self.size_average = size_average
-        self.elipson = 0.000001
+        if isinstance(alpha,list):
+            assert len(alpha)==num_classes   # α可以以list方式输入,size:[num_classes] 用于对不同类别精细地赋予权重
+            print("Focal_loss alpha = {}, 将对每一类权重进行精细化赋值".format(alpha))
+            self.alpha = torch.Tensor(alpha)
+        else:
+            assert alpha<1   #如果α为一个常数,则降低第一类的影响,在目标检测中为第一类
+            print(" --- Focal_loss alpha = {} ,将对背景类进行衰减,请在目标检测任务中使用 --- ".format(alpha))
+            self.alpha = torch.zeros(num_classes)
+            self.alpha[0] += alpha
+            self.alpha[1:] += (1-alpha) # α 最终为 [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
 
-    def forward(self, y_pred, y_true):
+        self.gamma = gamma
+
+    def forward(self, preds, labels):
         """
-        cal culates loss
-        logits: batch_size * labels_length * seq_length
-        labels: batch_size * seq_length
+        focal_loss损失计算
+        :param preds:   预测类别. size:[B,N,C] or [B,C]    分别对应与检测与分类任务, B 批次, N检测框数, C类别数
+        :param labels:  实际类别. size:[B,N] or [B]
+        :return:
         """
+        # assert preds.dim()==2 and labels.dim()==1
+        preds = preds.view(-1,preds.size(-1))
+        self.alpha = self.alpha.to(preds.device)
+        preds_softmax = F.softmax(preds, dim=1) # 这里并没有直接使用log_softmax, 因为后面会用到softmax的结果(当然你也可以使用log_softmax,然后进行exp操作)
+        preds_logsoft = torch.log(preds_softmax)
 
-        y_pred = y_pred + self.elipson
+        preds_softmax = preds_softmax.gather(1,labels.view(-1,1))   # 这部分实现nll_loss ( crossempty = log_softmax + nll )
+        preds_logsoft = preds_logsoft.gather(1,labels.view(-1,1))
+        self.alpha = self.alpha.gather(0,labels.view(-1))
+        loss = -torch.mul(torch.pow((1-preds_softmax), self.gamma), preds_logsoft)  # torch.pow((1-preds_softmax), self.gamma) 为focal loss中 (1-pt)**γ
 
-        # print("y_true.shape: ",y_true.shape)
-        # print("y_pred.shape: ",y_pred.shape)
-
-        cross_entropy = -y_true * torch.log(y_pred)
-
-        weight = torch.pow(1 - y_pred,self.gamma) * y_true
-
-        focalloss = cross_entropy * weight * self.alpha
-
-        reduce_focalloss = torch.max(focalloss)
-
-        return reduce_focalloss
-
-
+        loss = torch.mul(self.alpha, loss.t())
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+        return loss
 
 
 def weights_init(m):
