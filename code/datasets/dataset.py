8d210f1ae60b40b685ae152d50d28e9c0c714124
Xin-Zhang-IceMan <578789645@qq.com>
2020-07-10 21:06:14 +0800

    Add files via upload

--- code/datasets/dataset.py
+++ code/datasets/dataset.py
@@ -34,7 +34,8 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 class sparkset(data.Dataset):
     """
     """
-    def __init__(self, data_root_path, data_json_path, is_train=True, transform=None):
+
+    def __init__(self, data_root_path, data_json_path, is_train=True, transform=None, is_flip=True):
         # specify annotation file for dataset
         if is_train:
             self.data_root_path = data_root_path
@@ -48,9 +49,10 @@ class sparkset(data.Dataset):
         self.all_data_dict = get_info(self.data_root_path, self.data_json_path)
         self.is_train = is_train
         self.transform = transform
+        self.is_flip = is_flip
         # self.data_root = cfg.DATASET.ROOT
         self.input_size = [256, 256]
-        self.output_size = [64, 64]
+        self.output_size = [256, 256]
         self.sigma = 1.5
         # self.scale_factor = cfg.DATASET.SCALE_FACTOR
 
@@ -69,7 +71,31 @@ class sparkset(data.Dataset):
         return len(self.all_data_dict)
 
     def __getitem__(self, idx):
+        # idx 为图片索引
 
+        # image_path = os.path.join(self.data_root,
+        #                           self.landmarks_frame.iloc[idx, 0])
+        # print("img idx : %d"%idx)
+
+        output_size = [256, 256]
+        input_size = [256, 256]
+        sigma = 1.25
+
+        # result = get_info(trainPath, trainjsonPath)  #获取图片路径及对应的annotation
+
+        # result[:][:] = result
+        # result = np.squeeze(result)
+        # print(result[0])
+        # print(result[0][1][0]['data']['point'][0]['tag'])
+        # print(result[0][0])
+        # print(len(result))
+        # print(type(result))
+        # print(type(result))
+        # print(result)
+
+        shape_list = []
+
+        # img = cv2.imread(r"E:\BME\HRNet-Facial-Landmark-Detection-master\CEP\images\TrainData\051.bmp")
 
         # print(type(img))
         # print(img.shape)
@@ -78,14 +104,17 @@ class sparkset(data.Dataset):
         img = dicom2array(img_dir)  # 获取具体的图片数据，二维数据
         annotation = self.all_data_dict[idx][1]  # 获取图片的标签
         points = annotation[0]['data']['point']
+        pixel_spacing = self.all_data_dict[idx][2]  # 获取pixel-spacing
+        pixel_spacing = np.array(pixel_spacing.split('\\'), dtype='float')
         pts = []
         labels = []
         for point in points:
-            # print("point",point)
+            # print("point", point)
             coord = point['coord']
             tag = point["tag"]
+            idc = ide_dict[tag['identification']]
             # center_x, center_y =
-            coord_xy = [coord[0], coord[1]]
+            coord_xy = [coord[0], coord[1], idc]
             pts.append(coord_xy)
 
             if "disc" in tag.keys():
@@ -101,7 +130,9 @@ class sparkset(data.Dataset):
 
             # print(identification)
             # print(tag)
+        pts = sorted(pts, key=lambda x: x[2])
         pts = np.array(pts)
+        pts = pts[:, :2]
         pts = pts.astype('float').reshape(-1, 2)
         # print("pts.shape: ",pts.shape)
         # print("labels: ",labels)
@@ -110,32 +141,25 @@ class sparkset(data.Dataset):
         # 坐标也要随之变化
 
         height, weight = img.shape[0], img.shape[1]
-
+        ori_size = min(height, weight)
+        if random.random() < 0.5 and self.is_flip:
+            img = np.fliplr(img)
+            pts = np.array([[weight - i[0], i[1]] for i in pts])
         # print(img.shape)
         # print("before: ", img.shape)
         if weight > height:
-
-            extra_weight = weight - height
-            crop_left = int(extra_weight / 2) - 1
-            crop_right = int(extra_weight / 2) + height - 1
-
-            pts = pts - [0, crop_left]
             # 必须要用逗号索引
-            img = img[:, crop_left:crop_right]
-
+            img = img[:, :height]
 
         elif height > weight:
-            extra_height = height - weight
-            crop_up = int(extra_height / 2) - 1
-            crop_down = int(extra_height / 2) + weight - 1
-
-            pts = pts - [crop_up, 0]
-
-            img = img[crop_up:crop_down, :]
+            img = img[:weight, :]
+        # print(np.shape(img))
+        # cv2.imshow('', img)
+        # cv2.waitKey(0)
 
         img = Image.fromarray(img)
-        img = img.resize((self.input_size[0], self.input_size[1]))
-        pts *= [self.output_size[0] / self.input_size[0], self.output_size[1] / self.input_size[1]]
+        img = img.resize((input_size[0], input_size[1]))
+        pts *= [input_size[0] / ori_size, input_size[1] / ori_size]
 
         # print("img.size: ",img.size)
 
@@ -145,13 +169,13 @@ class sparkset(data.Dataset):
             print(img_dir)
         # print("nparts: ",nparts)
 
-        target = np.zeros((nparts, self.output_size[0], self.output_size[1]))
-        ##target 为生成的HeatMap   shape:19 x 64 x 64
+        target = np.zeros((nparts, output_size[0], output_size[1]))
+        ##target 为生成的HeatMap   shape:11 x 64 x 64
 
         # 自己添加的坐标变换，读入的明明是原图的坐标 但是generate heatmap的时候却用了64x64下的坐标
         # 是在transform_pixel的时候处理了
-        tpts = pts.copy() * [float(self.output_size[0]) / float(self.input_size[0]),
-                             float(self.output_size[0]) / float(self.input_size[0])]
+        tpts = pts.copy() * [float(output_size[0]) / float(input_size[0]),
+                             float(output_size[0]) / float(input_size[0])]
         ##复制一份Landmark坐标点
         ##transformed points
 
@@ -169,7 +193,7 @@ class sparkset(data.Dataset):
                 # 生成heatmap图 target[i] 表示第i个点的Heatmap
                 # sigma为超参数
                 # 在这部分可能有精度的损失,每次产生的Heatmap不同
-                target[i] = generate_target(target[i], tpts[i] - 1, config.sigma,
+                target[i] = generate_target(target[i], tpts[i] - 1, sigma,
                                             label_type=self.label_type)
                 # pass
         #
@@ -183,15 +207,10 @@ class sparkset(data.Dataset):
         #     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         #
         # shape_list.append(img.shape)
-        # img = np.asarray(img)
-        #
-        # # img = (img / 255.0 - self.mean) / self.std
-        # img = torch.Tensor(img)
-
-        if self.transform:
-            img = self.transform(img)
-
-        # img = img.unsqueeze(0)
+        img = np.asarray(img)
+        img = (img / 255.0 - self.mean) / self.std
+        img = torch.Tensor(img)
+        img = img.unsqueeze(0)
         target = torch.Tensor(target)
         tpts = torch.Tensor(tpts)  # 转化后所有的的坐标值
         # center = torch.Tensor(center) #当前
@@ -199,9 +218,49 @@ class sparkset(data.Dataset):
         # meta = {'index': idx, 'center': center, 'scale': scale,
         #         'pts': torch.Tensor(pts), 'tpts': tpts, 'box_size': box_size}
 
-        meta = {'index': idx, 'pts': torch.Tensor(pts), 'tpts': tpts}
+        meta = {'index': idx, 'pts': torch.Tensor(pts), 'tpts': tpts, 'ori': ori_size, 'ps': pixel_spacing}
 
         return img, target, meta
+
+
+class TestDataset(data.Dataset):
+    def __init__(self, test_path):
+        self.test_studies = construct_studies(test_path)
+        self.test_list = []
+        for study in self.test_studies.values():
+            self.test_list.append(study.t2_sagittal_middle_frame)
+        self.mean = np.array([0.485], dtype=np.float32)
+        self.std = np.array([0.229], dtype=np.float32)
+
+    def __len__(self):
+        return len(self.test_studies)
+
+    def __getitem__(self, item):
+        middle_frame = self.test_list[item]
+        img = middle_frame.image
+        img = np.asarray(img)
+        height, weight = img.shape[0], img.shape[1]
+        ori_size = min(height, weight)
+        # print(img.shape)
+        # print("before: ", img.shape)
+        if weight > height:
+            # 必须要用逗号索引
+            img = img[:, :height]
+
+        elif height > weight:
+            img = img[:weight, :]
+        else:
+            img = img
+        input_size = [256, 256]
+        img = Image.fromarray(img)
+        img = img.resize((input_size[0], input_size[1]))
+        img = np.asarray(img)
+        img = (img / 255.0 - self.mean) / self.std
+        img = torch.Tensor(img)
+        img = img.unsqueeze(0)
+        meta = {'ori': ori_size, 'study_uid': middle_frame.study_uid, 'series_uid': middle_frame.series_uid,
+                'instance_uid': middle_frame.instance_uid, 'path': middle_frame.file_path}
+        return img, meta
 
 class axialdataset(data.Dataset):
     """
