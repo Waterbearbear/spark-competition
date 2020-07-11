05c216b1897a7180938bb2be6bc5e0ff040f0b79
superIceBird <578789645@qq.com>
2020-07-11 17:55:22 +0800

    Update pre.py

--- code/pre.py
+++ code/pre.py
@@ -6,7 +6,12 @@ import numpy as np
 from model import Model
 from structure import construct_studies
 from utils.imgread import get_info, dicom2array
-from dataset import TestDataset
+from datasets.dataset import TestDataset
+
+ide = ['T12-L1', 'L1', 'L1-L2',
+       'L2', 'L2-L3', 'L3',
+       'L3-L4', 'L4', 'L4-L5',
+       'L5', 'L5-S1']
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -15,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--isTrain', type=bool, default=False)
     parser.add_argument('--continue_train', type=bool, default=False)
     parser.add_argument('--batchsize', type=int, default=1, help='input batch size')
-    parser.add_argument('--weights', type=int, default=249)
+    parser.add_argument('--weights', type=int, default=124)
     parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
     parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
     parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate for adam')
@@ -23,18 +28,25 @@ if __name__ == '__main__':
     testdataset = TestDataset('F:/DATA/Lumbar/lumbar_testA50/')
     model = Model(opt)
     dataloader = torch.utils.data.DataLoader(dataset=testdataset, batch_size=1, shuffle=False)
-    test_list = []
+    json_list = []
     for i, data in enumerate(dataloader):
         meta = data[1]
-        print(meta)
+        # print(meta)
         ori_img = dicom2array(meta['path'][0])
-        print(ori_img)
+        # print(ori_img)
         model.set_pre_input(data[0], data[1])
         model.test()
         _, preCoord = model.getPreCoord()
-        point_list = []
+        annotation = {"annotator": 13, "point": []}
         for j, coord in enumerate(preCoord):
-            point = {'coord': [coord[0], coord[1]],}
+            point = {"coord": [int(coord[0]), int(coord[1])],
+                     'tag': {'identification': ide[j], 'disc' if j % 2 == 0 else 'vertebra': None},
+                     'zIndex': int(meta['instance_number'][0]) - 1}
+            annotation['point'].append(point)
+        data_list = {"instanceUid": meta['instance_uid'][0], "seriesUid": meta['series_uid'][0],
+                     "annotation": [annotation]}
+        test_list = {"studyUid": meta['study_uid'][0], "data": [data_list]}
+        json_list.append(test_list)
         # point_size = 1
         # point_color = (0, 0, 255)  # BGR
         # thickness = 4  # 可以为 0 、4、8
@@ -43,7 +55,11 @@ if __name__ == '__main__':
         #     cv2.circle(ori_img, coord, point_size, point_color, thickness)
         # cv2.imshow('', ori_img)
         # cv2.waitKey(0)
-
+    print(json_list)
+    jsondata = json.dumps(json_list)
+    f = open('test.json', 'w')
+    f.write(jsondata)
+    f.close()
 
 
 
